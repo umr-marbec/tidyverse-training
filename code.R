cc6861c82cfea0d1dae2454626b3aa0a4c28b21a
MathieuDepetris <mathieu.depetris@cnrs.fr>
2025-03-25 14:56:10 +0100

    Add exercice 4.1 and 4.2 stringr section

--- code.R
+++ code.R
@@ -1,15 +1,15 @@
-# setup
+# setup ----
 library(readr)
 library(dplyr)
 library(stringr)
-# exercise 1.1
+# exercise 1.1 ----
 survey_metadata <- read_tsv(file = "data/survey_metadata.tsv")
 reef_fish_biomass <- read_delim(file = "data/reef_fish_biomass.dude",
                                 delim = "|")
 Sys.setenv("VROOM_CONNECTION_SIZE" = 2000000)
 reef_fish_abundance <- read_csv(file = "data/reef_fish_abundance.zip",
                                 guess_max = Inf)
-# exercise 1.2
+# exercise 1.2 ----
 function_1_2 <- function(x, pos) subset(x, country == "Indonesia" & depth < 20 & visibility > 10)
 survey_metadata_selection_1_2 <- read_tsv_chunked(file = "data/survey_metadata.tsv",
                                                   callback = DataFrameCallback$new(function_1_2),
@@ -30,7 +30,7 @@ biomass_exercice2_2 <- mutate(.data = biomass_exercice2_1_distinct_filter,
   relocate(survey_id_full,
            .before = survey_id) %>%
   select(-4,
-                -reporting_name) %>%
+         -reporting_name) %>%
   rename(size_class_cm = size_class) %>%
   mutate(size_class_mm = size_class_cm * 10) %>%
   select(-size_class_cm) %>%
@@ -51,42 +51,23 @@ survey_metadata_distinct <- distinct(.data = survey_metadata)
 biomass_survey_exercice2_4 <- biomass_exercice2_3 %>%
   left_join(survey_metadata_distinct,
             by = "survey_id")
-save(biomass_survey_exercice2_4,
-     file = "data/biomass_survey_exercice2_4.RData")
-# exercise 3.1 ----
-abundance <- read_delim(file = "./data/Global_reef_fish_abundance.csv")
-# exercise 3.2 ----
-abundance_long <- abundance %>%
-  pivot_longer(
-    .,
-    cols = 2:17523,
-    names_to = "sp_size",
-    values_to = "abundance",
-    values_drop_na = TRUE
-  ) %>%
-  unite(., col = "survey_id_full", c(survey_id, sp_size), sep = "_")
-slice_head(abundance_long)
-# exercise 3.3 ----
-biomass_abundance <- biomass_survey_exercice2_4 %>%
-  left_join(., abundance_long, by = "survey_id_full")
-# exercise 3.4 ----
-biomass_abundance <- biomass_abundance %>%
-  separate(., species_name, into = c("genus", "species"))
-slice_head(biomass_abundance[,8:11])
-# exercise 3.5 ----
-data_sf <- sf::st_as_sf(biomass_abundance,
-                        coords = c("longitude", "latitude") ,
-                        remove = FALSE,
-                        crs = "+proj=longlat +datum=WGS84 +ellps=WGS84 +towgs84=0,0,0")
-# exercise 3.6 ---
-biomass_survey_abundance_exercice3_6 <- biomass_abundance %>%
-  drop_na(latitude) %>% 
-  drop_na(longitude) %>%
-  sf::st_as_sf(
-    .,
-    coords = c("longitude", "latitude"),
-    remove = FALSE,
-    crs = "+proj=longlat +datum=WGS84 +ellps=WGS84 +towgs84=0,0,0"
-  )
-save(biomass_survey_abundance_exercice3_6,
-     file = "./data/biomass_survey_abundance_exercice3_6.RData")
+# exercice 4.1 ----
+biomass_survey_exercice4_1 <- slice(.data = biomass_survey_exercice2_4,
+                                    str_which(string = biomass_survey_exercice2_4$family,
+                                              pattern = "Embiotocidae"))
+sum_data_embiotoca_exercice4_1 <- sum(str_count(string = biomass_survey_exercice4_1$species_name,
+                                                pattern = "Embiotoca."))
+location_biomass_survey_exercice4_1 <- unique(str_subset(string = biomass_survey_exercice4_1$location,
+                                                         pattern = "British Columbia",
+                                                         negate = TRUE))
+# exercice 4.2 ----
+biomass_survey_exercice4_2 <- biomass_survey_exercice4_1 %>%
+  mutate(site_name_length_ori = str_length(site_name),
+         site_name_clean = str_squish(site_name),
+         site_name_clean = str_to_sentence(site_name_clean)) %>%
+  relocate(site_name_length_ori,
+           site_name_clean,
+           .after = site_name)
+# exercice 4.3 ----
+biomass_survey_exercice4_3 <- biomass_survey_exercice4_2
+  
